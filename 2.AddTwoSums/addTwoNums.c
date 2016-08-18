340e7e5acf766ad4d02095387b1086c54461bffe
jasonvfun <jasonvfang@gmail.com>
2016-08-18 23:43:31 +0800

    add external methods

--- 2.AddTwoSums/addTwoNums.c
+++ 2.AddTwoSums/addTwoNums.c
@@ -37,6 +37,10 @@ int ListNode_init(struct ListNode *Head)
 
 void print_list(struct ListNode *ListHead)
 {
+	if (!ListHead)
+	{
+		return;
+	}
 	struct ListNode *p = ListHead->next;
 	
 	while (p){
@@ -54,9 +58,64 @@ void print_list(struct ListNode *ListHead)
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) 
 {
-    struct ListNode *p1 = NULL, *p2 = NULL;
+    struct ListNode *p1 = NULL, *p2 = NULL, *m1 = NULL, *m2 = NULL;
+    struct ListNode *tmpNode = NULL;
+	p1 = l1; 
+	p2 = p1->next;
 	
+	m1 = l2;
+	m2 = m1->next;
 	
+	//return NULL;
+	
+	//alloc a head node
+	struct ListNode *retHeadList = malloc(sizeof(struct ListNode));
+	memset(retHeadList, 0, sizeof(struct ListNode));
+	
+	struct ListNode *lastNode = retHeadList;
+	
+	while(p2)
+	{
+		if (p2->next == NULL)//list 1 the last node
+		{
+			while(m2)
+			{
+				if (m2->next==NULL)
+				{ 
+					printf("Last L2 node:%d\n", m2->val);
+					break;
+				}
+				
+				m1 = m2;
+				m2 = m2->next;	
+			}
+			
+			if (m2)
+			{
+				tmpNode = malloc(sizeof(struct ListNode));
+				tmpNode->val = p2->val + m2->val;
+				tmpNode->next = NULL;
+				
+				//add to new list
+				lastNode->next = tmpNode;
+				lastNode = tmpNode;	
+				
+				free(p2);
+				free(m2);
+				
+				//reset to header.
+				p1->next= NULL;
+				p2 = l1;
+				
+				m1->next= NULL;
+				m2 = l2;	
+			}
+		}
+		p1 = p2;
+		p2 = p2->next;		
+	}
+	
+	return retHeadList;
 }
 
 int main()
@@ -64,9 +123,15 @@ int main()
 	ListNode_init(&ListHead_1);
 	ListNode_init(&ListHead_2);
 	
+	printf("List12:\n");
 	print_list(&ListHead_1);
+	
+	printf("List2:\n");
 	print_list(&ListHead_2);
+	
+	struct ListNode *newList = addTwoNumbers(&ListHead_1, &ListHead_2);
+	printf("New List:\n");
+	print_list(newList);
 	
 	return 0;
 }
-

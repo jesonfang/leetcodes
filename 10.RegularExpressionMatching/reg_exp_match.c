47788965a22f493b7c72d1118b476b87581864e9
Jason.fang <jianjiang.fang@linkplay.com>
2019-09-30 09:48:30 +0800

    Add tmp file

---
+++ 10.RegularExpressionMatching/reg_exp_match.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+typedef unsigned char  bool;
+
+#define true 1
+#define false 0
+
+
+bool isMatch(char * s, char * p){
+    bool ret = false;
+
+    return ret;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    
+    printf("isMatch=%d\n", isMatch(argv[1], argv[2]));
+
+    /* code */
+    return 0;
+}
+
